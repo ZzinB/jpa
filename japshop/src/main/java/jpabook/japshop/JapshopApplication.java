af96a21fabd17da6e811b0d8a9f62ad51d2d2aad
ZzinB <mysteleee7@gmail.com>
2024-06-05 10:42:50 +0900

    Settings: 프로젝트 생성

--- japshop/src/main/java/jpabook/japshop/JapshopApplication.java
+++ japshop/src/main/java/jpabook/japshop/JapshopApplication.java
@@ -1,13 +1,13 @@
-package jpabook.jpashop;
+package jpabook.japshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JpashopApplication {
+public class JapshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JpashopApplication.class, args);
+		SpringApplication.run(JapshopApplication.class, args);
 	}
 
 }
