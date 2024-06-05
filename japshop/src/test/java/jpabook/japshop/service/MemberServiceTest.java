ca22ecd292e1f293baa782ea9d624abeaabfc39b
ZzinB <mysteleee7@gmail.com>
2024-06-05 11:27:50 +0900

    Test: 회원(가입, 중복)

---
+++ japshop/src/test/java/jpabook/japshop/service/MemberServiceTest.java
@@ -0,0 +1,49 @@
+package jpabook.japshop.service;
+
+import jpabook.japshop.domain.Member;
+import jpabook.japshop.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberServiceTest {
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception{
+        //Given
+        Member member = new Member();
+        member.setName("lee");
+
+        //When
+        Long saveId = memberService.join(member);
+
+        //Then
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void 중복_회원_예외() throws Exception{
+        //Given
+        Member member1 = new Member();
+        member1.setName("lee");
+
+        Member member2 = new Member();
+        member2.setName("lee");
+
+        //When
+        memberService.join(member1);
+        memberService.join(member2); //예외발생
+
+        //Then
+        fail("예외 발생");
+    }
+
+}
