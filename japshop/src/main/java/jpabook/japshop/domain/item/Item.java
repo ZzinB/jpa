72b8f2983b783852027f8f38443560937b54363b
ZzinB <mysteleee7@gmail.com>
2024-06-07 13:05:03 +0900

    Feat: 상품 개발(등록, 찾기)

--- japshop/src/main/java/jpabook/japshop/domain/item/Item.java
+++ japshop/src/main/java/jpabook/japshop/domain/item/Item.java
@@ -24,4 +24,17 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<Category>();
+
+    //비즈니스 로직
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    public void removeSrock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
